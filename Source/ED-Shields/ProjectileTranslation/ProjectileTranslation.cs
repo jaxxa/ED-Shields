9b38a03a9a82af9f3c8abec87d833fd313861c17
Joshua W <jaxxa90@gmail.com>
2018-04-04 20:04:33 +1000

    WIP

---
+++ Source/ED-Shields/ProjectileTranslation/ProjectileTranslation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace EnhancedDevelopment.Shields.Basic.ProjectileTranslation
+{
+    abstract class ProjectileTranslation
+    {
+
+        public List<ProjectileTranslation> m_Translations = new List<ProjectileTranslation>();
+        
+        static ProjectileTranslation GetSpecificTranslator(Thing projectileThing)
+        {
+            return null;
+        }
+        
+        public abstract bool Destroyed();
+
+        public abstract bool FlyOverhead();
+
+        public abstract Thing Launcher();
+
+        public abstract bool WillTargetLandInRange();
+
+        public abstract Quaternion ExactRotation();
+
+        public abstract Vector3 ExactPosition();
+
+        public abstract int DamageAmountBase();
+
+        public abstract Thing ProjectileThing();
+    }
+}
---
+++ Source/ED-Shields/ProjectileTranslation/ProjectileTranslation_ProjectileCE.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace EnhancedDevelopment.Shields.Basic.ProjectileTranslation
+{
+    class ProjectileTranslation_ProjectileCE : ProjectileTranslation
+    {
+        public override int DamageAmountBase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Destroyed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Vector3 ExactPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Quaternion ExactRotation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool FlyOverhead()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Thing Launcher()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Thing ProjectileThing()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool WillTargetLandInRange()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
