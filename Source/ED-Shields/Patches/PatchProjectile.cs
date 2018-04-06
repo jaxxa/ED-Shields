4df4cabb63ed458d988931edd85940c64b381560
Joshua W <jaxxa90@gmail.com>
2018-04-06 17:58:26 +1000

    Adding Tick Override

--- Source/ED-Shields/Patches/PatchProjectile.cs
+++ Source/ED-Shields/Patches/PatchProjectile.cs
@@ -14,36 +14,12 @@ namespace EnhancedDevelopment.Shields
 
         protected override void ApplyPatch(HarmonyInstance harmony = null)
         {
-            //Get the Origional Resting Property
-            //ConstructorInfo[] _RimWorld_Plant_Resting = typeof(Verse.Projectile).GetConstructors();
-            //Log.Message("Count:" + _RimWorld_Plant_Resting.Count().ToString());
-            // ConstructorInfo _Temp = _RimWorld_Plant_Resting.FirstOrDefault();
-            // .GetProperty("Resting", BindingFlags.NonPublic | BindingFlags.Instance);
-            // Patcher.LogNULL(_RimWorld_Plant_Resting, "RimWorld_Plant_Resting");
 
+            this.ApplyLaunchPatch(harmony);
+            this.ApplyTickPatch(harmony);
 
-
-
-            //Get the Resting Property Getter Method
-            //MethodInfo _RimWorld_Plant_Resting_Getter = _RimWorld_Plant_Resting.GetGetMethod(true);
-            //Patcher.LogNULL(_RimWorld_Plant_Resting_Getter, "RimWorld_Plant_Resting_Getter");
-
-
-            Type[] _TypeArray = new Type[] { typeof(Verse.Thing), typeof(Vector3), typeof(LocalTargetInfo), typeof(Thing), typeof(Thing) };
-            //Get the Method
-            //MethodInfo _ProjectileLaunch = typeof(Verse.Projectile).GetMethod("Launch", BindingFlags.Public | BindingFlags.Instance);
-            MethodInfo _ProjectileLaunch = typeof(Verse.Projectile).GetMethod("Launch", _TypeArray);
-            Patcher.LogNULL(_ProjectileLaunch, "_ProjectileLaunch");
-
-
-            //Get the Prefix Patch
-            MethodInfo _RestingGetterPrefix = typeof(PatchProjectile).GetMethod("RestingGetterPrefix", BindingFlags.Public | BindingFlags.Static);
-            Patcher.LogNULL(_RestingGetterPrefix, "_PatchProjectile.Method");
-
-            //Apply the Prefix Patch
-            harmony.Patch(_ProjectileLaunch, new HarmonyMethod(_RestingGetterPrefix), null);
         }
-
+        
         protected override string PatchDescription()
         {
             return "PatchProjectile";
@@ -55,11 +31,31 @@ namespace EnhancedDevelopment.Shields
             //return Mod_EnhancedOptions.Settings.Plant24HEnabled;
         }
 
+
+        #region "Launch Patch"
+
+        private void ApplyLaunchPatch(HarmonyInstance harmony)
+        {
+
+            //Get the Launch Method
+            Type[] _TypeArray = new Type[] { typeof(Verse.Thing), typeof(Vector3), typeof(LocalTargetInfo), typeof(Thing), typeof(Thing) };
+            MethodInfo _ProjectileLaunch = typeof(Verse.Projectile).GetMethod("Launch", _TypeArray);
+            Patcher.LogNULL(_ProjectileLaunch, "_ProjectileLaunch");
+
+            //Get the Launch Prefix Patch
+            MethodInfo _ProjectileLaunchPrefix = typeof(PatchProjectile).GetMethod("ProjectileLaunchPrefix", BindingFlags.Public | BindingFlags.Static);
+            Patcher.LogNULL(_ProjectileLaunchPrefix, "_ProjectileLaunchPrefix");
+
+            //Apply the Prefix Patches
+            harmony.Patch(_ProjectileLaunch, new HarmonyMethod(_ProjectileLaunchPrefix), null);
+        }
+
+
         // prefix
         // - wants instance, result and count
         // - wants to change count
         // - returns a boolean that controls if original is executed (true) or not (false)
-        public static Boolean RestingGetterPrefix()
+        public static Boolean ProjectileLaunchPrefix()
         {
             Log.Message("Created Projectile");
             //This is the result that will be used, note that it was passed as a ref.
@@ -69,5 +65,41 @@ namespace EnhancedDevelopment.Shields
             return true;
         }
 
+        #endregion
+
+
+        #region "Tick Patch"
+
+        private void ApplyTickPatch(HarmonyInstance harmony)
+        {
+
+            //Get the Launch Method
+            MethodInfo _ProjectileTick = typeof(Verse.Projectile).GetMethod("Tick");
+            Patcher.LogNULL(_ProjectileTick, "_ProjectileTick");
+
+            //Get the Launch Prefix Patch
+            MethodInfo _ProjectileTickPrefix = typeof(PatchProjectile).GetMethod("ProjectileTickPrefix", BindingFlags.Public | BindingFlags.Static);
+            Patcher.LogNULL(_ProjectileTickPrefix, "_ProjectileTickPrefix");
+
+            //Apply the Prefix Patches
+            harmony.Patch(_ProjectileTick, new HarmonyMethod(_ProjectileTickPrefix), null);
+        }
+
+
+        // prefix
+        // - wants instance, result and count
+        // - wants to change count
+        // - returns a boolean that controls if original is executed (true) or not (false)
+        public static Boolean ProjectileTickPrefix()
+        {
+            Log.Message("Ticking Projectile");
+            //This is the result that will be used, note that it was passed as a ref.
+            //  __result = false;
+
+            //Retuen False so the origional method is not executed, overriting the false result.
+            return true;
+        }
+
+        #endregion
     }
 }
