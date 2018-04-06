a6bccc8080818afbb67d79fe43a060fc1e5a0d18
Joshua W <jaxxa90@gmail.com>
2018-04-06 17:47:59 +1000

    Adding initial Projectile Patching

---
+++ Source/ED-Shields/Patches/Patcher.cs
@@ -0,0 +1,55 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace EnhancedDevelopment.Shields
+{
+    [StaticConstructorOnStartup]
+    internal class Patcher
+    {
+        static Patcher()
+        {
+            string _LogLocation = "EnhancedOptions.Patcher.Patcher(): ";
+
+            Log.Message(_LogLocation + "Starting.");
+
+            //Create List of Patches
+            List<Patch> _Patches = new List<Patch>();
+            _Patches.Add(new PatchProjectile());
+
+            //Create Harmony Instance
+            HarmonyInstance _Harmony = HarmonyInstance.Create("EnhancedDevelopment.WarningOptions");
+
+            //Iterate Patches
+            _Patches.ForEach(p => p.ApplyPatchIfRequired(_Harmony));
+
+            Log.Message(_LogLocation + "Complete.");
+        }
+
+        /// <summary>
+        /// Debug Logging Helper
+        /// </summary>
+        /// <param name="objectToTest"></param>
+        /// <param name="name"></param>
+        /// <param name="logSucess"></param>
+        public static void LogNULL(object objectToTest, String name, bool logSucess = false)
+        {
+            if (objectToTest == null)
+            {
+                Log.Error(name + " Is NULL.");
+            }
+            else if (logSucess)
+            {
+                Log.Message(name + " Is Not NULL.");
+            }
+        }
+
+    }
+
+}
