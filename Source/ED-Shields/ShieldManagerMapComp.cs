263a4d167701d2f4c5b251fbe5fbead663908b35
Joshua W <jaxxa90@gmail.com>
2018-04-06 17:35:21 +1000

    MapComp Test.

---
+++ Source/ED-Shields/ShieldManagerMapComp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace EnhancedDevelopment.Shields.Basic
+{
+    class ShieldManagerMapComp : MapComponent
+    {
+
+        private List<Projectile> m_Projectiles = new List<Projectile>();
+
+        public ShieldManagerMapComp(Map map) : base(map)
+        {
+         //   map.components.<ShieldManagerMapComp>();
+        }
+
+        public override void MapComponentTick()
+        {
+            base.MapComponentTick();
+            Log.Message("MapCompTick");
+        }
+    }
+}
