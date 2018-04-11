0a4962062dd6aab0aa9f42f1764a70fa8bd444f7
Joshua W <jaxxa90@gmail.com>
2018-04-11 18:09:46 +1000

    Adding visual effects when blocking bullets

--- Source/ED-Shields/ShieldManagerMapComp.cs
+++ Source/ED-Shields/ShieldManagerMapComp.cs
@@ -1,20 +1,27 @@
 ﻿using EnhancedDevelopment.Shields.Basic;
+using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
 using Verse;
+using Verse.Sound;
 
 namespace EnhancedDevelopment.Shields.Basic
 {
+    [StaticConstructorOnStartup]
     class ShieldManagerMapComp : MapComponent
     {
+
+
+        public static readonly SoundDef HitSoundDef = SoundDef.Named("Shields_HitShield");
 
         private List<Projectile> m_Projectiles = new List<Projectile>();
 
         public ShieldManagerMapComp(Map map) : base(map)
         {
+            this.map = map;
             //   map.components.<ShieldManagerMapComp>();
         }
 
@@ -49,6 +56,12 @@ namespace EnhancedDevelopment.Shields.Basic
             }))
             {
                 Log.Message("Blocked");
+
+                //On hit effects
+                MoteMaker.ThrowLightningGlow(projectile.ExactPosition, this.map, 0.5f);
+                //On hit sound
+                HitSoundDef.PlayOneShot((SoundInfo)new TargetInfo(projectile.Position, projectile.Map, false));
+
                 projectile.Destroy();
                 return true;
             }
