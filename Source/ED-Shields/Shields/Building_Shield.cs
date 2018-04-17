bc59e3bbce7fab9d46623bb024bc73f64e46dcd5
Joshua W <jaxxa90@gmail.com>
2018-04-17 21:37:35 +1000

    Moving some logic into another Method

--- Source/ED-Shields/Shields/Building_Shield.cs
+++ Source/ED-Shields/Shields/Building_Shield.cs
@@ -472,99 +472,115 @@ namespace EnhancedDevelopment.Shields.Basic
             }
             List<Thing> things = this.Map.thingGrid.ThingsListAt(square);
             List<Thing> thingsToDestroy = new List<Thing>();
-            Boolean _IFFCheck = this.m_StructuralIntegrityMode;
 
             for (int i = 0, l = things.Count(); i < l; i++)
             {
 
                 if (things[i] != null && things[i] is Projectile)
                 {
-                    //Assign to variable
-                    Projectile pr = (Projectile)things[i];
-                    if (!pr.Destroyed && ((this.m_BlockIndirect_Avalable && this.m_BlockDirect_Active && pr.def.projectile.flyOverhead) || (this.m_BlockDirect_Avalable && this.m_BlockIndirect_Active && !pr.def.projectile.flyOverhead)))
+                    if (this.ShouldDestroyProjectileVanilla(things[i]))
                     {
-                        bool wantToIntercept = true;
-
-                        //Check IFF
-                        if (_IFFCheck == true)
-                        {
-                            //Log.Message("IFFcheck == true");
-                            Thing launcher = ReflectionHelper.GetInstanceField(typeof(Projectile), pr, "launcher") as Thing;
-
-                            if (launcher != null)
-                            {
-                                if (launcher.Faction != null)
-                                {
-                                    //Log.Message("launcher != null");
-                                    if (launcher.Faction.IsPlayer)
-                                    {
-                                        wantToIntercept = false;
-                                    }
-                                    else
-                                    {
-
-                                    }
-                                }
-                            }
-                        }
-
-                        //Check OverShoot
-                        if (pr.def.projectile.flyOverhead)
-                        {
-                            if (this.WillTargetLandInRange(pr))
-                            {
-                                //Log.Message("Fly Over");
-                            }
-                            else
-                            {
-                                wantToIntercept = false;
-                                //Log.Message("In Range");
-                            }
-                        }
-
-
-                        if (wantToIntercept)
-                        {
-
-                            //Detect proper collision using angles
-                            Quaternion targetAngle = pr.ExactRotation;
-
-                            Vector3 projectilePosition2D = pr.ExactPosition;
-                            projectilePosition2D.y = 0;
-
-                            Vector3 shieldPosition2D = Vectors.IntVecToVec(this.Position);
-                            shieldPosition2D.y = 0;
-
-                            Quaternion shieldProjAng = Quaternion.LookRotation(projectilePosition2D - shieldPosition2D);
-
-
-                            if ((Quaternion.Angle(targetAngle, shieldProjAng) > 90))
-                            {
-
-                                //On hit effects
-                                MoteMaker.ThrowLightningGlow(pr.ExactPosition, this.Map, 0.5f);
-                                //On hit sound
-                                HitSoundDef.PlayOneShot((SoundInfo)new TargetInfo(this.Position, this.Map, false));
-
-                                //Damage the shield
-                                ProcessDamage(pr.def.projectile.damageAmountBase);
-                                //add projectile to the list of things to be destroyed
-                                thingsToDestroy.Add(pr);
-                            }
-                        }
-                        else
-                        {
-                            //Log.Message("Skip");
-                        }
-
+                        thingsToDestroy.Add(things[i]);
                     }
                 }
+
+
+
             }
             foreach (Thing currentThing in thingsToDestroy)
             {
                 currentThing.Destroy();
             }
 
+        }
+
+
+        private bool ShouldDestroyProjectileVanilla(Thing projectileThing)
+        {
+            Boolean _IFFCheck = this.m_StructuralIntegrityMode;
+
+            //Assign to variable
+            Projectile pr = (Projectile)projectileThing;
+            if (!pr.Destroyed && ((this.m_BlockIndirect_Avalable && this.m_BlockDirect_Active && pr.def.projectile.flyOverhead) || (this.m_BlockDirect_Avalable && this.m_BlockIndirect_Active && !pr.def.projectile.flyOverhead)))
+            {
+                bool wantToIntercept = true;
+
+                //Check IFF
+                if (_IFFCheck == true)
+                {
+                    //Log.Message("IFFcheck == true");
+                    Thing launcher = ReflectionHelper.GetInstanceField(typeof(Projectile), pr, "launcher") as Thing;
+
+                    if (launcher != null)
+                    {
+                        if (launcher.Faction != null)
+                        {
+                            //Log.Message("launcher != null");
+                            if (launcher.Faction.IsPlayer)
+                            {
+                                wantToIntercept = false;
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                    }
+                }
+
+                //Check OverShoot
+                if (pr.def.projectile.flyOverhead)
+                {
+                    if (this.WillTargetLandInRange(pr))
+                    {
+                        //Log.Message("Fly Over");
+                    }
+                    else
+                    {
+                        wantToIntercept = false;
+                        //Log.Message("In Range");
+                    }
+                }
+
+
+                if (wantToIntercept)
+                {
+
+                    //Detect proper collision using angles
+                    Quaternion targetAngle = pr.ExactRotation;
+
+                    Vector3 projectilePosition2D = pr.ExactPosition;
+                    projectilePosition2D.y = 0;
+
+                    Vector3 shieldPosition2D = Vectors.IntVecToVec(this.Position);
+                    shieldPosition2D.y = 0;
+
+                    Quaternion shieldProjAng = Quaternion.LookRotation(projectilePosition2D - shieldPosition2D);
+
+
+                    if ((Quaternion.Angle(targetAngle, shieldProjAng) > 90))
+                    {
+
+                        //On hit effects
+                        MoteMaker.ThrowLightningGlow(pr.ExactPosition, this.Map, 0.5f);
+                        //On hit sound
+                        HitSoundDef.PlayOneShot((SoundInfo)new TargetInfo(this.Position, this.Map, false));
+
+                        //Damage the shield
+                        ProcessDamage(pr.def.projectile.damageAmountBase);
+                        //add projectile to the list of things to be destroyed
+
+                        return true;
+                    }
+                }
+                else
+                {
+                    //Log.Message("Skip");
+                }
+
+            }
+            
+            return false;
         }
 
         /// <summary>
