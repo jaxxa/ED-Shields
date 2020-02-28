69c01b23d68b2511f88cb7c03f8145024eacdf19
Martyn Green <martyn.robert.green@gmail.com>
2020-02-28 20:25:19 +0000

    Update compatability to include 1.1

---
+++ ED-ShieldsBasic/1.1/Source/ED-Shields/Shields/Comp_ShieldBuilding.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace EnhancedDevelopment.Shields.Basic
+{
+    public class Comp_ShieldBuilding : ThingComp
+    {
+    }
+}
